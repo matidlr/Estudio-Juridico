3f975039d904ea7cb41b44eb9e9f02394ebc41fb
matidlr <argentinavende.sa@gmail.com>
2026-07-02 02:38:49 -0300

    Actualizacion

---
+++ EstudioJuridico.API/Models/Actualizacion.cs
@@ -0,0 +1,17 @@
+// Models/Actualizacion.cs
+// Cada novedad que el abogado carga en el caso.
+public class Actualizacion
+{
+    public int Id { get; set; }
+    public string Contenido { get; set; } = string.Empty;
+    public DateTime Fecha { get; set; } = DateTime.UtcNow;
+
+    public int CasoId { get; set; }
+    public Caso Caso { get; set; } = null!;
+
+    // Quién la escribió (siempre el abogado)
+    public int AutorId { get; set; }
+    public Usuario Autor { get; set; } = null!;
+
+    public List<Archivo> Archivos { get; set; } = new();
+}
--- EstudioJuridico.API/obj/Debug/net10.0/EstudioJuridico.API.AssemblyInfo.cs
+++ EstudioJuridico.API/obj/Debug/net10.0/EstudioJuridico.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstudioJuridico.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201334f2f5af972eb9724c0f5c312b29abc33119")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d68591d09abb0ddc1e9be7be741e320cd1a8bc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstudioJuridico.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstudioJuridico.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
