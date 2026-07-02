04843b4130ff30b116b4e2b489afaa9a8b9f7157
matidlr <argentinavende.sa@gmail.com>
2026-07-02 03:00:01 -0300

    Auth controller

--- EstudioJuridico.API/obj/Debug/net10.0/EstudioJuridico.API.AssemblyInfo.cs
+++ EstudioJuridico.API/obj/Debug/net10.0/EstudioJuridico.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstudioJuridico.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f583407258ffaa353417f9abed05da195c38727b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722547293a1fda659b112d0228fbe639a7ceb095")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstudioJuridico.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstudioJuridico.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
