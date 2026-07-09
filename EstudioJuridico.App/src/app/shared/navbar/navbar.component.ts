a0ea31baf8db25b36e36329454f948fb79869df3
matidlr <argentinavende.sa@gmail.com>
2026-07-09 20:37:28 -0300

    navbar component

--- EstudioJuridico.App/src/app/shared/navbar/navbar.component.ts
+++ EstudioJuridico.App/src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(public authService: AuthService, private router: Router) {}
 
+  logout() {
+    this.authService.logout();
+  }
 }
