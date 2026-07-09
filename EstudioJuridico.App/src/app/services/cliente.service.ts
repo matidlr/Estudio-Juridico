b9a4d9e3de7289e7cc1e2f00d814b8fb247f081d
matidlr <argentinavende.sa@gmail.com>
2026-07-09 20:34:20 -0300

    client service

--- EstudioJuridico.App/src/app/services/cliente.service.ts
+++ EstudioJuridico.App/src/app/services/cliente.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTodosClientes() {
+    return this.http.get<any[]>(`${this.apiUrl}/clientes`);
+  }
+
+  getClientePorId(id: number) {
+    return this.http.get<any>(`${this.apiUrl}/clientes/${id}`);
+  }
+
+  getMiPerfil() {
+    return this.http.get<any>(`${this.apiUrl}/clientes/miperfil`);
+  }
+
+  actualizarMiPerfil(datos: any) {
+    return this.http.put(`${this.apiUrl}/clientes/miperfil`, datos);
+  }
 }
