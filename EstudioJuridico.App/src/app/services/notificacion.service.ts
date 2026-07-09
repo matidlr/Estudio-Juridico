2f617a7d37c15468b16ab9dbf5e8ef7c8137d088
matidlr <argentinavende.sa@gmail.com>
2026-07-09 20:35:08 -0300

    notificacion service

--- EstudioJuridico.App/src/app/services/notificacion.service.ts
+++ EstudioJuridico.App/src/app/services/notificacion.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificacionService {
+  private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPreferencias() {
+    return this.http.get<any>(`${this.apiUrl}/notificaciones/preferencias`);
+  }
+
+  actualizarPreferencias(preferencias: any) {
+    return this.http.put(`${this.apiUrl}/notificaciones/preferencias`, preferencias);
+  }
 }
