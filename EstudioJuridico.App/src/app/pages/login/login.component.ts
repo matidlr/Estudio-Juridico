87d53ba3d0a143170257dc5238ccf0f38d10328b
matidlr <argentinavende.sa@gmail.com>
2026-07-09 20:42:59 -0300

    login ts

--- EstudioJuridico.App/src/app/pages/login/login.component.ts
+++ EstudioJuridico.App/src/app/pages/login/login.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink, NavbarComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+  error = '';
+  cargando = false;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.error = '';
+    this.cargando = true;
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: (res) => {
+        this.authService.guardarToken(res.token);
+        this.router.navigate(['/cliente/panel']);
+      },
+      error: () => {
+        this.error = 'Email o contraseña incorrectos.';
+        this.cargando = false;
+      }
+    });
+  }
 }
