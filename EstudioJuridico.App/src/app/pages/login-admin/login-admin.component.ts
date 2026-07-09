1969c4ed2f130879a788e35b0526a3b909802425
matidlr <argentinavende.sa@gmail.com>
2026-07-09 20:47:58 -0300

    login admin ts

--- EstudioJuridico.App/src/app/pages/login-admin/login-admin.component.ts
+++ EstudioJuridico.App/src/app/pages/login-admin/login-admin.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-admin',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink, NavbarComponent],
   templateUrl: './login-admin.component.html',
   styleUrl: './login-admin.component.scss'
 })
 export class LoginAdminComponent {
+  email = '';
+  password = '';
+  error = '';
+  cargando = false;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.error = '';
+    this.cargando = true;
+
+    this.authService.loginAdmin(this.email, this.password).subscribe({
+      next: (res) => {
+        this.authService.guardarToken(res.token);
+
+        if (this.authService.getRol() === 'Admin') {
+          this.router.navigate(['/admin/panel']);
+        } else {
+          this.authService.logout();
+          this.error = 'No tenés permisos de administrador.';
+          this.cargando = false;
+        }
+      },
+      error: () => {
+        this.error = 'Email o contraseña incorrectos.';
+        this.cargando = false;
+      }
+    });
+  }
 }
