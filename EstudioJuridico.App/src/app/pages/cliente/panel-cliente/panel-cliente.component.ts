687ae8cc7ae719be9f62cff9521a02de229023b9
matidlr <argentinavende.sa@gmail.com>
2026-07-09 20:10:40 -0300

    panel cliemt component

---
+++ EstudioJuridico.App/src/app/pages/cliente/panel-cliente/panel-cliente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-cliente',
+  imports: [],
+  templateUrl: './panel-cliente.component.html',
+  styleUrl: './panel-cliente.component.scss'
+})
+export class PanelClienteComponent {
+
+}
