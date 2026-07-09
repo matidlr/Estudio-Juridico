878019c9b0767ebf92ca8ce88942e0a24f85c55a
matidlr <argentinavende.sa@gmail.com>
2026-07-09 20:30:11 -0300

    admin guard

--- EstudioJuridico.App/src/app/guards/admin.guard.ts
+++ EstudioJuridico.App/src/app/guards/admin.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
-export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
+export const adminGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+  const token = localStorage.getItem('token');
+
+  if (token && authService.getRol() === 'Admin') {
+    return true;
+  }
+
+  router.navigate(['/login-admin']);
+  return false;
 };
