403ea470a9ea73f31e945ebbd2c5308d6eba868d
matidlr <argentinavende.sa@gmail.com>
2026-07-09 20:09:44 -0300

    login admin component

---
+++ EstudioJuridico.App/src/app/pages/login-admin/login-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-admin',
+  imports: [],
+  templateUrl: './login-admin.component.html',
+  styleUrl: './login-admin.component.scss'
+})
+export class LoginAdminComponent {
+
+}
