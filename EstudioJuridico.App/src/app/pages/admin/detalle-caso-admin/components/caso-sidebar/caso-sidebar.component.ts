ca53ceabe73efd0179eebf83bf6aa2f68caf6c81
matidlr <argentinavende.sa@gmail.com>
2026-07-11 11:36:34 -0300

    sidebar

--- EstudioJuridico.App/src/app/pages/admin/detalle-caso-admin/components/caso-sidebar/caso-sidebar.component.ts
+++ EstudioJuridico.App/src/app/pages/admin/detalle-caso-admin/components/caso-sidebar/caso-sidebar.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-caso-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './caso-sidebar.component.html',
   styleUrl: './caso-sidebar.component.scss'
 })
 export class CasoSidebarComponent {
+  @Input() caso: any = null;
+  @Input() seccionActiva = 'info';
+  @Input() archivosCount = 0;
+  @Input() pruebasCount = 0;
+  @Input() recordatoriosCount = 0;
 
+  @Output() seccionCambiada = new EventEmitter<string>();
+
+  cambiarSeccion(seccion: string) {
+    this.seccionCambiada.emit(seccion);
+  }
 }
