4060b0f7d0f5498e22390f486028dd5ec531c65d
matidlr <argentinavende.sa@gmail.com>
2026-07-12 17:16:45 -0300

    admin sidebar ts

--- EstudioJuridico.App/src/app/shared/admin-sidebar/admin-sidebar.component.ts
+++ EstudioJuridico.App/src/app/shared/admin-sidebar/admin-sidebar.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-admin-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './admin-sidebar.component.html',
   styleUrl: './admin-sidebar.component.scss'
 })
 export class AdminSidebarComponent {
+  constructor(public authService: AuthService, private router: Router) {}
 
+  get rutaActiva(): string {
+    return this.router.url;
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
