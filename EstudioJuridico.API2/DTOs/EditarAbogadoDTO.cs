31a07a2770f508d67fd4654975c93f2d353d1c34
matidlr <argentinavende.sa@gmail.com>
2026-07-12 18:05:23 -0300

    edit abogado DTO

---
+++ EstudioJuridico.API2/DTOs/EditarAbogadoDTO.cs
@@ -0,0 +1,7 @@
+public class EditarAbogadoDTO
+{
+    public string Nombre      { get; set; } = string.Empty;
+    public string Apellido    { get; set; } = string.Empty;
+    public string Matricula   { get; set; } = string.Empty;
+    public string Especialidad { get; set; } = string.Empty;
+}
