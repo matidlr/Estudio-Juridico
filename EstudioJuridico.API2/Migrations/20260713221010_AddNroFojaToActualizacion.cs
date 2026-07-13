3c911b997070707b3a9b65667880bb6cfc05ae08
matidlr <argentinavende.sa@gmail.com>
2026-07-13 19:11:05 -0300

    .

---
+++ EstudioJuridico.API2/Migrations/20260713221010_AddNroFojaToActualizacion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstudioJuridico.API2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNroFojaToActualizacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NroFoja",
+                table: "Actualizaciones",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NroFoja",
+                table: "Actualizaciones");
+        }
+    }
+}
