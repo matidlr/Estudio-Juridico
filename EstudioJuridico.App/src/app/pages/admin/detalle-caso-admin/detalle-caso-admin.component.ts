2101fc8a631af6e72fc148664d5c86114cbfadc5
matidlr <argentinavende.sa@gmail.com>
2026-07-14 09:12:16 -0300

    add fojas button

--- EstudioJuridico.App/src/app/pages/admin/detalle-caso-admin/detalle-caso-admin.component.ts
+++ EstudioJuridico.App/src/app/pages/admin/detalle-caso-admin/detalle-caso-admin.component.ts
@@ -69,6 +69,9 @@ creandoRecordatorio = false;
   busquedaFoja = '';
   actualizacionesFiltradas: any[] = [];
 
+  mostrarFormFoja = false;
+  mostrarFormSeccion = false;
+
   constructor(
     private route: ActivatedRoute,
     private casoService: CasoService,
