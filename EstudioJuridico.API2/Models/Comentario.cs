094b5ab766028886177151399e178338bbb362ac
matidlr <argentinavende.sa@gmail.com>
2026-07-16 19:58:21 -0300

    add tipo Autor to comentario

--- EstudioJuridico.API2/Models/Comentario.cs
+++ EstudioJuridico.API2/Models/Comentario.cs
@@ -1,16 +1,14 @@
-// Models/Comentario.cs
 public class Comentario
 {
     public int Id { get; set; }
     public string Texto { get; set; } = string.Empty;
     public DateTime Fecha { get; set; } = DateTime.UtcNow;
-
-    // true = el abogado puede verlo, false = es una nota privada del cliente
     public bool VisibleAlAbogado { get; set; } = true;
-
     public int CasoId { get; set; }
     public Caso Caso { get; set; } = null!;
-
     public int UsuarioId { get; set; }
     public Usuario Usuario { get; set; } = null!;
+
+    // Para saber si es respuesta del abogado o consulta del cliente
+    public string TipoAutor { get; set; } = "Cliente";
 }
