341ce11d5b5952c2529710434646cb2972993fc2
matidlr <argentinavende.sa@gmail.com>
2026-07-17 02:00:59 -0300

    comentario model

--- EstudioJuridico.API2/Models/Comentario.cs
+++ EstudioJuridico.API2/Models/Comentario.cs
@@ -4,11 +4,10 @@ public class Comentario
     public string Texto { get; set; } = string.Empty;
     public DateTime Fecha { get; set; } = DateTime.UtcNow;
     public bool VisibleAlAbogado { get; set; } = true;
+    public string TipoAutor { get; set; } = "Cliente";
+    public bool Leida { get; set; } = false;
     public int CasoId { get; set; }
     public Caso Caso { get; set; } = null!;
     public int UsuarioId { get; set; }
     public Usuario Usuario { get; set; } = null!;
-
-    // Para saber si es respuesta del abogado o consulta del cliente
-    public string TipoAutor { get; set; } = "Cliente";
 }
