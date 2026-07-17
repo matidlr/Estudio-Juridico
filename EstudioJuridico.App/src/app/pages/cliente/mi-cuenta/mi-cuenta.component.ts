4a22058b869dc77ac90381c3cb6503e1cbd6a08c
matidlr <argentinavende.sa@gmail.com>
2026-07-17 14:23:45 -0300

    mi cuenta component

--- EstudioJuridico.App/src/app/pages/cliente/mi-cuenta/mi-cuenta.component.ts
+++ EstudioJuridico.App/src/app/pages/cliente/mi-cuenta/mi-cuenta.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ClienteSidebarComponent } from '../../../shared/cliente-sidebar/cliente-sidebar.component';
+import { CasoService } from '../../../services/caso.service';
 
 @Component({
   selector: 'app-mi-cuenta',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, ClienteSidebarComponent],
   templateUrl: './mi-cuenta.component.html',
   styleUrl: './mi-cuenta.component.scss'
 })
-export class MiCuentaComponent {
+export class MiCuentaComponent implements OnInit {
+  movimientos: any[] = [];
+  movimientosFiltrados: any[] = [];
+  resumen: any = null;
+  cargando = true;
+  error = '';
+  filtroTipo = 'todos';
 
+  constructor(private casoService: CasoService) {}
+
+  ngOnInit() {
+    this.casoService.getMisMovimientos().subscribe({
+      next: (data) => {
+        this.movimientos = data.movimientos;
+        this.movimientosFiltrados = data.movimientos;
+        this.resumen = data.resumen;
+        this.cargando = false;
+      },
+      error: () => {
+        this.error = 'Error al cargar los movimientos.';
+        this.cargando = false;
+      }
+    });
+  }
+
+  filtrar() {
+    if (this.filtroTipo === 'todos') {
+      this.movimientosFiltrados = this.movimientos;
+    } else {
+      this.movimientosFiltrados = this.movimientos.filter(
+        m => m.tipo === this.filtroTipo
+      );
+    }
+  }
 }
