b8b6d8a9736da5583c80cf5418357df6ba39fa0b
matidlr <argentinavende.sa@gmail.com>
2026-07-16 21:04:21 -0300

    admin sidebar

--- EstudioJuridico.App/src/app/shared/admin-sidebar/admin-sidebar.component.ts
+++ EstudioJuridico.App/src/app/shared/admin-sidebar/admin-sidebar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { CasoService } from '../../services/caso.service';
 
 @Component({
   selector: 'app-admin-sidebar',
@@ -10,12 +11,13 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './admin-sidebar.component.html',
   styleUrl: './admin-sidebar.component.scss'
 })
-export class AdminSidebarComponent {
+export class AdminSidebarComponent implements OnInit {
   nombreUsuario = '';
   emailUsuario = '';
   iniciales = 'AD';
+  consultasPendientes = 0;
 
-  constructor(public authService: AuthService, private router: Router) {
+  constructor(public authService: AuthService, private router: Router, private casoService: CasoService) {
     const token = authService.getToken();
     if (token) {
       try {
@@ -25,6 +27,15 @@ export class AdminSidebarComponent {
         this.iniciales = this.nombreUsuario.substring(0, 2).toUpperCase();
       } catch {}
     }
+  }
+
+  ngOnInit() {
+    this.casoService.getConsultasPendientes().subscribe({
+      next: (consultas) => {
+        this.consultasPendientes = consultas.filter((c: any) => !c.tieneRespuesta).length;
+      },
+      error: () => {}
+    });
   }
 
   get rutaActiva(): string {
