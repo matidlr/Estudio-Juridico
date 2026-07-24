f47e2372f50c6f3a16bec0e805e34defc4252591
matidlr <argentinavende.sa@gmail.com>
2026-07-24 20:11:52 -0300

    Add HTTP security headers

--- EstudioJuridico.API2/Program.cs
+++ EstudioJuridico.API2/Program.cs
@@ -177,6 +177,16 @@ app.UseStaticFiles();
 app.UseCors("Angular");
 app.UseIpRateLimiting();
 app.UseMiddleware<TokenBlacklistMiddleware>();
+// Headers de seguridad HTTP
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Append("X-Frame-Options", "DENY");
+    context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
+    context.Response.Headers.Append("Referrer-Policy", "strict-origin-when-cross-origin");
+    context.Response.Headers.Append("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
+    await next();
+});
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
