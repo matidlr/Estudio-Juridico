adf9389aa6ceb66bbcea7b0a5328203bd64c2434
matidlr <argentinavende.sa@gmail.com>
2026-07-27 03:09:09 -0300

    Improve navbar and footer design, add contact info to footer

--- EstudioJuridico.App/src/app/shared/footer/footer.component.ts
+++ EstudioJuridico.App/src/app/shared/footer/footer.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  constructor(public authService: AuthService) {}
+}
